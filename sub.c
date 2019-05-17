91d2c6536ace974cc6d194d8d8d1d589edae2200
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-17 20:56:08 +0000

    fix sub after div

--- sub.c
+++ sub.c
@@ -10,10 +10,13 @@ int sup(void)
 	if (e.nel > 1)
 	{
 		tmp = e.matrix[e.nel - 1];
+		if (tmp != 0)
+		{
 		e.matrix[e.nel - 1] = '\0';
 		e.matrix[e.nel - 2] = e.matrix[e.nel - 2] - tmp;
 		e.nel = e.nel - 1;
 		return (0);
+		}
 	}
 	fprintf(stderr, "L%d: can't sub, stack too short\n", e.nline);
 	return (EXIT_FAILURE);
