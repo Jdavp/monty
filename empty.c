aa9bc25ed143b187424979ab93f0f40d9d4ba2ed
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-17 04:04:09 +0000

    first empty

---
+++ empty.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+/**
+ * empty - fn print error when not find functios allow
+ * Return: EXIT_FAILURE
+ */
+int empty(void)
+{
+	fprintf(stderr, "L%d: unknown instruction %s\n", e.nline, e.command);
+	return (EXIT_FAILURE);
+}
