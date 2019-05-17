a9cfd0eaa4f3fd9f4cda847c6fe459a4dd4d6c98
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-17 04:29:49 +0000

    after betty

--- add.c
+++ add.c
@@ -7,12 +7,14 @@
 int add(void)
 {
 	int tmp;
+
+
 	if (e.nel > 1)
 	{
 		tmp = e.matrix[e.nel - 1];
 		e.matrix[e.nel - 1] = '\0';
 		e.matrix[e.nel - 2] += tmp;
-		e.nel -=1;
+		e.nel -= 1;
 		return (0);
 	}
 	fprintf(stderr, "L%d: can't add, stack too short\n", e.nline);
