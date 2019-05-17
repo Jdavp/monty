80f936843b1ee400a51e2f44c1fd4f784db0f9de
Juan Diego Alejandro Valencia Pena <juandiegoalejandro@hotmail.com>
2019-05-17 21:09:57 +0000

    change function name to sub

--- sub.c
+++ sub.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
-* sup - fn subtracts the top element of the stack from the second top
+* sub - fn subtracts the top element of the stack from the second top
 * Return: 0 succesfull or EXIT_FAILURE failed
 */
-int sup(void)
+int sub(void)
 {
 	int tmp;
 
@@ -12,10 +12,10 @@ int sup(void)
 		tmp = e.matrix[e.nel - 1];
 		if (tmp != 0)
 		{
-		e.matrix[e.nel - 1] = '\0';
-		e.matrix[e.nel - 2] = e.matrix[e.nel - 2] - tmp;
-		e.nel = e.nel - 1;
-		return (0);
+			e.matrix[e.nel - 1] = '\0';
+			e.matrix[e.nel - 2] = e.matrix[e.nel - 2] - tmp;
+			e.nel = e.nel - 1;
+			return (0);
 		}
 	}
 	fprintf(stderr, "L%d: can't sub, stack too short\n", e.nline);
